2802927cc477cebc782b952b22e19d50cc871e54
Yann.Fohom <62346692+yann-fohom@users.noreply.github.com>
2021-07-05 01:47:35 +0300

    Create exo.py

---
+++ exo.py
@@ -0,0 +1,8 @@
+# Write a program in python that displays and count all even numbers between 1 and 100:
+even_number = 0
+for number in range(1,100):
+  if number % 2 == 0:
+  even_number +=
+  print(number)
+print(f"we have {even_number} even numbers between 1 and 100")
+
