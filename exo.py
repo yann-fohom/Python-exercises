4877f8ba2c49d42c691aad0c0951835158959fd7
Yann.Fohom <62346692+yann-fohom@users.noreply.github.com>
2021-07-05 01:53:17 +0300

    Update exo.py

--- exo.py
+++ exo.py
@@ -5,5 +5,5 @@ for number in range(1,100):
   even_number +=
   print(number)
 print(f"we have {even_number} even numbers between 1 and 100")
-# Bonus: for odd numbers just modify the if statement and check all gthe numbers wher the modulo (%) is = 1, ie: number%2 ==1
+# Bonus: for odd numbers just modify the if statement and check all the numbers where the modulo (%) is = 1, ie: number%2 ==1
 
