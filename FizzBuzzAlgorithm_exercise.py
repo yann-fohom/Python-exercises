2784fb697664c7071d51e9df7079ba8ef24c6b08
Yann.Fohom <62346692+yann-fohom@users.noreply.github.com>
2021-07-22 20:55:46 +0300

    Create FizzBuzzAlgorithm_exercise.py
    FizzBuzz

---
+++ FizzBuzzAlgorithm_exercise.py
@@ -0,0 +1,15 @@
+def fizz_buzz(input): 
+
+    if input == 3:
+        print ("Fizz")
+
+    elif input == 5:
+        print ("Buzz") 
+
+    elif input == (3 * 5):
+        print ("FizzBuzz") 
+
+    else:
+        print (input) 
+
+fizz_buzz(1)
