68ed3fc7e45e562f02fd029c91d0af65484b7505
Yann.Fohom <62346692+yann-fohom@users.noreply.github.com>
2021-07-26 23:17:13 +0300

    Delete exo.py

--- exo.py
+++
@@ -1,9 +0,0 @@
-# Write a program in python that displays and count all even numbers between 1 and 10:
-count = 0
-for number in range(1, 10):
-    if number % 2 == 0:
-        count += 1
-        print(number)
-print(f"we have {count} even numbers between 1 and 10") 
-# Bonus: for odd numbers just modify the if statement and check all the numbers where the modulo (%) is = 1, ie: number%2 ==1
-
